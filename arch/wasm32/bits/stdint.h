010561d568a40d4ab64ed8e0fd3692f09db6d802
Anvo Network <dev@anvo.network>
2026-03-17 21:14:37 +0000

    Fix missing fast integer typedefs and internal unistd.h
    - Add int_fast16_t, int_fast32_t, uint_fast16_t, uint_fast32_t typedefs
      to bits/stdint.h (were only limit macros, missing actual types)
    - Simplify src/include/unistd.h internal header (remove off_t-dependent
      declarations for OS functions not available in WASM)

--- arch/wasm32/bits/stdint.h
+++ arch/wasm32/bits/stdint.h
@@ -2,6 +2,11 @@
 typedef __int128 int128_t;
 typedef unsigned __int128 uint128_t;
 
+typedef int32_t  int_fast16_t;
+typedef int32_t  int_fast32_t;
+typedef uint32_t uint_fast16_t;
+typedef uint32_t uint_fast32_t;
+
 #define INT_FAST16_MIN  INT32_MIN
 #define INT_FAST32_MIN  INT32_MIN
 
